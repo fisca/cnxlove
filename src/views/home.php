4709adef3c3a15e0cd154a7d504b822adf319024
Suttipong Srikok <fisca2012@gmail.com>
2013-08-12 13:44:36 +0700

    edit article for testing and upload to the site

--- src/views/home.php
+++ src/views/home.php
@@ -7,6 +7,8 @@
         <meta name="viewport" content="width=device-width; initial-scale=1.0">
 
         <title>CNXLOVE.com</title>
+        
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 
         <!-- main css -->
         <link href="../themes/adaptive-design/css/style.css" rel="stylesheet" type="text/css">
@@ -37,35 +39,53 @@
                 <article class="post clearfix">
 
                     <header>
-                        <h1 class="post-title"><a href="#">Hello World.</a></h1>
-                        <p class="post-meta"><time class="post-date" datetime="2013-08-12" pubdate>Aug 12, 2013</time> <em>in</em> <a href="#">Category</a></p>
+                        <h1 class="post-title"><a href="#">ประชาชนแห่ซื้อดอกมะลิไหว้วันแม่</a></h1>
+                        <p class="post-meta"><time class="post-date" datetime="2013-08-11" pubdate>Aug 11, 2013</time> <em>in</em> <a href="#">Category</a></p>
                     </header>
                     <figure class="post-image"> 
-                        <img src="../themes/adaptive-design/images/sample-image.jpg" /> 
+                        <img src="../uploads/2013/08/post-2-0-68109800-1376214543.jpg" /> 
                     </figure>
-                    <p>Fusce ut sem est. In eu sagittis felis. In gravida arcu ut neque ornare vitae rutrum turpis vehicula. Nunc ultrices sem mollis metus rutrum non malesuada metus fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque interdum rutrum quam, a pharetra est pulvinar ac. Vestibulum congue nisl magna. Ut vulputate odio id dui convallis in adipiscing libero condimentum. Nunc et pharetra enim. Praesent pharetra, neque et luctus tempor, leo sapien faucibus leo, a dignissim turpis ipsum sed libero. Sed sed luctus purus. Aliquam faucibus turpis at libero consectetur euismod. Nam nunc lectus, congue non egestas quis, condimentum ut arcu. Nulla placerat, tortor non egestas rutrum, mi turpis adipiscing dui, et mollis turpis tortor vel orci. Cras a fringilla nunc. Suspendisse volutpat, eros congue scelerisque iaculis, magna odio sodales dui, vitae vulputate elit metus ac arcu. Mauris consequat rhoncus dolor id sagittis. Cras tortor elit, aliquet quis tincidunt eget, dignissim non tortor.</p>
+                    <p>
+                        ประชาชนแห่ซื้อดอกมะลิเพื่อนำไปไหว้แม่สื่อความรักในวันแม่แห่งชาติปีนี้ 
+                        เผยราคาแพงขึ้นจากปีที่ผ่านมาเนื่องจากพายุฝนถล่มดอกมะลิเน่า ราคาจึงแพงขึ้นกว่าปีที่ผ่านมามากขึ้น 
+                        แต่บรรยากาศการซื้อดอกมะลิยังคึกคักอยู่                        
+                    </p>
+                    <p>
+                    	 เมื่อวันที่ 11 ส.ค.56 สำหรับบรรยากาศก่อนวันแม่แห่งชาติประจำปีนี้ ในจังหวัดเชียงใหม่ประชาชนร่วมออกซื้อดอกมะลิ ซึ่งเป็นดอกไม้สัญลักษณ์สำคัญที่ใช้แสดงความรักที่มีต่อแม่กันแล้ว โดยที่บริเวณตลาดต้นลำไยซึ่งเป้นย่านค้าดอกไม้แห่งใหญ่ในเขตอำเภอเมืองเชียงใหม่ มีประชาชน นักเรียนนักศึกษาจำนวนมากเริ่มออกมาซื้อดอกมะลิเพื่อนำไปมอบให้แม่ในวันแม่แห่งชาติประจำปีนี้กันแล้ว โดยบรรยากาศขายดอกมะลิปีนี้เป็นไปอย่างคึกคักแม้ว่าจะมีราคาเพิ่มจากปีที่ผ่านมา โดยพ่อค้าแม่ค้านำเอามะลิเป็นพวงมาขาย พร้อมพวงมาลัยดอกมะลิ โดยพ่อค้าแม่ค้าก็มีการดีไซด์รูปแบบเพื่อให้แปลกใหม่แทนที่จะเป็นพวงธรรมดา เป็นรูปหัวใจที่สื่อถึงความรักที่สวยงามด้วย
+                    </p>
+                    <p>
+                    	 สำหรับราคามะลิปีนี้ขึ้นราคาจากปีที่ผ่านมา โดยราคาขายร้อยสร้อยเป็นพวงจำนวน 10 พวง ขายราคา 150 บาท ตกราคาเส้นละ 15 บาท จากปีที่ผ่านมาขายเพียง 120 บาท ส่วนพวงมาลัยนั้นราคาเดิมปีที่แล้ว 120 บาท แต่ปีนี้ขายราคา 150 บาทขึ้นไป โดยดอกมะลิปีนี้ที่มาราคาแพงนั้นเนื่องจากออกมาน้อยเพราะถูกพายุฝนจนทำให้ดอกมะลิเน่า และดอกมะลิที่ได้ออกมาจำนวนน้อยไม่เพียงพอต่อความต้องการของตลาดราคาจึงแพงขึ้น
+                    </p>
+                 	<p>
+                    	<small>ภาพและข่าวโดยทีมข่าว CM108.com .</small>
+              		</p>
 
-                    <h3>Vimeo Video</h3>
-                    <div class="video">
-                        <iframe src="http://player.vimeo.com/video/6284199" width="550" height="400" frameborder="0"></iframe>
-                    </div>
-                    <!-- /.video -->
+                </article>
+                <!-- /.post -->
+                
+                <hr />
+                <article class="post clearfix">
 
-                    <h3>YouTube</h3>
-                    <div class="video">
-                        <object width="500" height="400">
-                            <param name="movie" value="http://www.youtube.com/v/NmRTreaCJXs?version=3">
-                            </param>
-                            <param name="allowFullScreen" value="true">
-                            </param>
-                            <param name="allowscriptaccess" value="always">
-                            </param>
-                            <param name="wmode" value="transparent">
-                            </param>
-                            <embed wmode="transparent"  src="http://www.youtube.com/v/NmRTreaCJXs?version=3" type="application/x-shockwave-flash" width="600" height="420" allowscriptaccess="always" allowfullscreen="true"></embed>
-                        </object>
-                    </div>
-                    <!-- /.video -->
+                    <header>
+                        <h1 class="post-title"><a href="#">รายงานสถานการณ์น้ำ ในเชียงใหม่ มีน้ำไหลจากเชียงดาวมาในเมืองเชียงใหม่</a></h1>
+                        <p class="post-meta"><time class="post-date" datetime="2013-08-11" pubdate>Aug 11, 2013</time> <em>in</em> <a href="#">Category</a></p>
+                    </header>
+                    <figure class="post-image"> 
+                        <img src="../uploads/2013/08/post-2-0-97556300-1376213856.jpg"> 
+                    </figure>
+                    <p>
+                       11 สิงหาคม 2556 หัวหน้าสำนักงานป้องกันและบรรเทาสาธารณภัยจังหวัดเชียงใหม่ รายงานปริมาณน้ำ หลังเกิดภาวะฝนตกต่อเนื่องในจังหวัดเชียงใหม่ โดยน้ำจากอำเภอเชียงดาวได้ไหลลงมาสู่ตัวเมืองเชียงใหม่แล้ว ยังสามารถควบคุมได้ ขณะที่ประตูระบายน้ำที่ตำบลป่าแดด เปิดประตูระบายน้ำปิงแล้ว ทั้งหมด 6 บาน                        
+                    </p>
+                    <p>
+                    	 นายคมสัน สุวรรณอัมพา หัวหน้าสำนักงานป้องกันและบรรเทาสาธารณภัยจังหวัดเชียงใหม่ เปิดเผยถึงสถานการณ์น้ำในจังหวัดเชียงใหม่ จากที่ตกต่อเนื่องระยะนี้ และตกตลอดทั้งคืนจนถึงวันนี้(11 ส.ค.56) ว่า ในพื้นที่อำเภอเมืองด้านทิศตะวันตก บริเวณดอยสุเทพ บ้านขุนช่างเคี่ยน มีปริมาณน้ำฝนสะสม 24 ชั่วโมง ตั้งแต่เมื่อคืน เกิน 100 มิลลิเมตร จังหวัดได้ประสานกับนายกองค์การบริหารส่วนตำบลช้างเผือก เข้าดำเนินการ เป็นไปด้วยความเรียบร้อย อีกหลายพื้นที่ ได้แก่ อำเภอจอมทอง แม่วางและแม่ริมบางส่วน ขณะนี้ มีปริมาณน้ำสะสมค่อนข้างมาก ทางจังหวัดได้เฝ้าดูอยู่ คาดว่าจะมีน้ำที่ไหลจากดอยสุเทพลงมา ทำให้น้ำในคันคลองชลประทานเพิ่มสูงขึ้น ซึ่งขณะนี้มีปริมาณมากอยู่แล้ว ทางชลประทานได้เร่งระบายน้ำ เพื่อเตรียมรองรับน้ำใหม่ โดยภายในตัวอำเภอเมือง ยังมีน้ำขังอยู่บ้างในพื้นที่ลุ่มต่ำ เช่น บริเวณศรีปิงเมือง ซอย 5 เทศบาลนครเชียงใหม่ ได้เร่งสูบน้ำออกแล้ว ขณะที่ อำเภอแม่ริมด้านหน้าบริเวณกองพันสัตว์ต่าง ลักษณะเป็นพื้นที่ลุ่มต่ำ ทำให้เกิดน้ำท่วมขัง มีปัญหาด้านการสัญจร องค์การบริหารส่วนตำบลดอนแก้ว ได้ดำเนินการอำนวยความสะดวกแล้ว สำหรับ น้ำจากอำเภอเชียงดาวได้ไหลลงมาสู่ตัวเมืองเชียงใหม่และยังสามารถควบคุมได้ สำหรับ ประตูระบายน้ำตำบลป่าแดด อำเภอเมือง ได้ดำเนินการเปิดประตูระบายน้ำทั้งหมด 6 บาน นั่นคือ ให้ปริมาณแม่น้ำปิงไหลทั้งหมด 100 % ไม่มีการบังคับน้ำ และระยะนี้ ต้องเตรียมรับมือ พายุ อูตอร์ ที่อาจส่งผลกระทบกับจังหวัดเชียงใหม่ เชียงรายได้
+                    </p>
+                    <p>
+                    	 หัวหน้าสำนักงานป้องกันและบรรเทาสาธารณภัยจังหวัดเชียงใหม่ กล่าวด้วยว่า สำหรับพื้นที่ที่ได้รับความเสียหายจากอุทกภัย ได้แก่ พื้นที่ตำบลแม่เหียะ ประชาชนได้รับความเดือดร้อนจากน้ำป่าไหลผ่าน ในหมู่ที่ 5 ประมาณ 20 หลังคาเรือน ได้รับการช่วยเหลือแล้ว ส่วนอำเภอทางเหนือของจังหวัดเชียงใหม่ ที่มีน้ำท่วมขังระยะนี้ สถานการณ์คลี่คลายแล้ว คงเหลือแต่อำเภอแม่อาย ที่ยังมีปริมาณน้ำมาก ขณะที่ มูลนิธิราชประชานุเคราะห์ ได้เข้ามอบสิ่งของพระราชทานฯ ให้กับประชาชน ในพื้นที่อำเภอฝางและไชยปราการ อำเภอละ 500 ชุด
+                    </p>
+                 	<p>
+                    	<small>ข่าวโดย   กนกรัตน์ ปัญญา สวท. เชียงใหม่ <br />ภาพโดยคุณ Ake CNX</small>
+              		</p>
+                    
 
                 </article>
                 <!-- /.post -->
