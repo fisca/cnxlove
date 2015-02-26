7577701a03482ee4935c19e5736313f5e1be9a5e
fisca <fisca2012@gmail.com>
2015-02-26 16:35:38 +0700

    Input wp_ functions.

--- wp-content/themes/cnxlove/footer.php
+++ wp-content/themes/cnxlove/footer.php
@@ -2,3 +2,8 @@
 <footer>
     &copy; 2011 <?php echo date('Y'); ?> cnxlove.com
 </footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- wp-content/themes/cnxlove/header.php
+++ wp-content/themes/cnxlove/header.php
@@ -1,8 +1,38 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package Cnxlove
+ * @subpackage Cnxlove
+ * @since Cnxlove 1.0
  */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+        
+        <link rel="stylesheet" href="">
+        
+        <!--[if lt IE 9]>
+        <script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/html5.js"></script>
+        <![endif]-->
+        <script>
+            (function () {
+                document.documentElement.className = 'js';
+            })();
+        </script>        
+        
+        <?php wp_head(); ?>
+    </head>
 
+    <body <?php body_class(); ?>>
+        <header class="container">
+            <h2><?php bloginfo('name'); ?></h2>
+            <p><?php bloginfo('description'); ?></p>            
+        </header>
+        
